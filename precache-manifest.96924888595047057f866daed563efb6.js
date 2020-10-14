c1e8639128f2790bb8bbb82b9eae24eb356fc908
Spoorthy Rajashekar <57374095+SpoorthyKR@users.noreply.github.com>
2020-10-14 19:59:25 -0400

    Add files via upload

---
+++ precache-manifest.96924888595047057f866daed563efb6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cff55134c10719b435f40dce10294526",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c36f0393a974f7755d3d",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "eae86e57cd69d81bf459",
+    "url": "/static/js/2.3498522d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.3498522d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c36f0393a974f7755d3d",
+    "url": "/static/js/main.df2eaeab.chunk.js"
+  },
+  {
+    "revision": "1550bc5cb6224d84350f",
+    "url": "/static/js/runtime-main.74ac9606.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
